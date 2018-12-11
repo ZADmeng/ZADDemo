8d6ea96e5847c048ebd47bf539c021c77a5e4d59
wangdongyi <wangdongyi@zmeng123.com>
2018-12-11 15:14:47 +0800

    Initial commit

---
+++ app/src/main/java/com/zad/demo/BaseApplication.java
@@ -0,0 +1,18 @@
+package com.zad.demo;
+
+import android.app.Application;
+
+import com.zad.sdk.main.ZADSdk;
+
+
+/**
+ * 作者：王东一
+ * 创建时间：2018/9/20.
+ */
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ZADSdk.init(getApplicationContext(),"zm_app_1000","8QEYqLu7bcqFGGoE");
+    }
+}
